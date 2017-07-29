450a90aab671232d94e091f7743e5a5577a679ff
Michael Bradley <mbradleyis@gmail.com>
2017-07-29 05:23:58 -0700

    added logo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import deepEqual from 'deep-equal';
 import marked from 'marked';
 import moment from 'moment';
 import steem from 'steem';
+import logo from './assets/steem.png';
 
 class App extends Component {
 
@@ -74,10 +75,11 @@ componentDidUpdate(){
 
   renderPosts(){
     const { posts } = this.state;
+    const type = this.state.type === "Created" ? "new" : this.state.type.toLowerCase();
     return (<div>
           {this.state.query && posts.length ? (<div style={{padding: 10}}><a href={`https://steemit.com/${this.state.type.toLowerCase()}/${this.state.query.toLowerCase()}`} target="_blank">
-            View {this.state.type === "Created" ? "new" : "trending"} results for "{this.state.query}" on steemit
-          </a></div>) : this.state.query ? <div style={{padding: 10}}>No results found for {this.state.query}</div> : null}
+            View {type} results for "{this.state.query}" on steemit
+          </a></div>) : this.state.query ? <div style={{padding: 10}}>No {type} results found for "{this.state.query}"</div> : null}
             {posts.map(post => {
             const metadata = JSON.parse(post.json_metadata);
             const image = metadata.image;
@@ -85,25 +87,30 @@ componentDidUpdate(){
             return  (
               <table style={{margin: 10, padding: 10, height: 100, maxHeight: 100, width: "100%", borderBottom: "1px solid lightgray"}}>
                 <tbody>
-                  {!this.state.nsfw && tags.includes("nsfw") ? <tr><td>This post is not safe for work</td></tr> : (
+                  {!this.state.nsfw && tags && tags.includes("nsfw") ? <tr><td>This post is not safe for work <select onChange={(e)=>this.handleNSFWChange(e.target.value)}>
+                    <option selected={this.state.nsfw} value="false">Hide NSFW Posts</option>
+                    <option selected={this.state.nsfw} value="true">Show NSFW Posts</option>
+                  </select></td></tr> : (
                       <tr>
-                        <td style={{maxHeight: 100, maxWidth: 100, width: 100, overflow: "hidden"}}>
+                        <td style={{position: "relative", height: 100, maxHeight: 100, maxWidth: 100, width: 100, overflow: "hidden"}}>
                         {
                           image
-                            ? <img style={{maxHeight: 100}} src={image[0]}/>
+                            ? <a href={`https://steemit.com${post.url}`} target="_blank"><img style={{position: "absolute", top: 5, maxHeight: 80}} src={image[0]}/></a>
                             : null
                         }
                       </td>
-                      <td>
+                      <td style={{position: "relative", verticalAlign: "middle"}}>
+                        <div style={{position: "absolute", top: 5}}>
+                    <h3 style={{marginTop: 0}}>
+                    <a style={{color: "#000000", textDecoration: "none"}} href={`https://steemit.com${post.url}`} target="_blank">{post.title}</a>
+                  </h3>
                     <div>
-                    {post.title}
-                  </div>
-                    <div>
-                      pending payout ${post.pending_payout_value} | {post.active_votes.length} {post.active_votes.length === 1 ? "vote" : "votes"} | {post.replies.length} {post.replies.length === 1 ? "comment" : "comments"} | <a href={post.url} target="_blank">view on steemit</a>
+                      ${post.pending_payout_value} | {post.active_votes.length} {post.active_votes.length === 1 ? "vote" : "votes"} | {post.replies.length} {post.replies.length === 1 ? "comment" : "comments"} | <a href={`https://steemit.com${post.url}`} target="_blank">view post on steemit</a>
                   </div>
                   <div>
-                    posted by <a href={`https://steemit.com/@${post.author}`} target="_blank">{`@${post.author}`}</a> on {moment(post.active).format('LLLL')}
+                    posted by <a href={`https://steemit.com/@${post.author}`} target="_blank">{`@${post.author}`}</a> in <a href={`https://steemit.com/${this.state.type.toLowerCase()}/${tags && tags[0] ? tags[0] : '?' }`} target="_blank"> {tags && tags[0] ? tags[0] : '?'}</a> on {moment(post.active).format('LLLL')}
                   </div>
+                </div>
                 </td>
               </tr>)}
       </tbody></table>)})
@@ -114,26 +121,28 @@ componentDidUpdate(){
     console.log(this.state.posts)
     return (
       <div>
-        <div style={{borderBottom: "1px solid lightgray", width: window.screen.width}}>
-          <div style={{padding: 10}}>
-            Search Steem
-          </div>
-          <div style={{padding: 10}}>
-              <input onChange={(e)=>this.handleQueryChange(e.target.value)} type="text" value={this.state.query} placeholder="Tag (one word only)" />
+        <div style={{padding: 10, borderBottom: "1px solid lightgray", width: window.screen.width}}>
+          <span>
+            <img style={{height: 40, verticalAlign: "middle", paddingRight: 10}} src={logo} />
+        </span>
+          <span>
+              <input style={{width: 300}} onChange={(e)=>this.handleQueryChange(e.target.value)} type="text" value={this.state.query} placeholder="Search a Tag (one word only)" />
             <select onChange={(e)=>this.handleTypeChange(e.target.value)}>
-                <option value="Created">New</option>
-                <option value="Trending">Trending</option>
-              </select>
+              <option value="Created">New</option>
+              <option value="Hot">Hot</option>
+              <option value="Trending">Trending</option>
+              <option value="Promoted">Promoted</option>
+            </select>
               <select onChange={(e)=>this.handleLimitChange(e.target.value)}>
                 <option value="10">10 Posts</option>
-              <option value="50">50 Posts</option>
-            <option value="100">100 Posts</option>
+                <option value="50">50 Posts</option>
+                <option value="100">100 Posts</option>
               </select>
               <select onChange={(e)=>this.handleNSFWChange(e.target.value)}>
-                <option value="false">Hide NSFW Posts</option>
-              <option value="true">show NSFW Posts</option>
+                <option selected={this.state.nsfw} value="false">Hide NSFW Posts</option>
+                <option selected={this.state.nsfw} value="true">Show NSFW Posts</option>
               </select>
-          </div>
+          </span>
         </div>
         {this.renderPosts()}
       </div>
